1e4e43b9b2098979cc056bd81c0b8c31aa6c156c
Enver Toleuov <enver.toleuov@mail.com>
2022-04-12 15:24:38 +0500

    v2

--- api/getCurrency.php
+++ api/getCurrency.php
@@ -1,9 +1,5 @@
 <?php
-
-declare(strict_types=1);
 require_once($_SERVER['DOCUMENT_ROOT'] . '/class/Currency.php');
-
-$arr = json_decode($_POST['JSON_data']);
 
 $curr = new Currency;
 echo json_encode($curr->get());
